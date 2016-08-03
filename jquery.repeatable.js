9f8d1d0b00103130a2bee5cd84661d898cb46203
David Buchweitz <davidbuchweitz@gmail.com>
2016-08-03 09:44:02 +0100

    Add files via upload

---
+++ jquery.repeatable.js
@@ -0,0 +1,40 @@
+/*
+	jQuery Mouse Click Event Repeater
+	https://github.com/davidbuchweitz/repeater
+*/
+
+jQuery.fn.repeatable = function()
+{
+	var step_delay = 300;
+	var divisor = 1.25;
+	var min_speed = 50;
+
+	var timer = null;
+
+	$(this).each(function()
+	{
+		var obj = $(this);
+
+		obj.on('mousedown', function()
+		{
+			interval = step_delay;
+
+			var stepper = function()
+			{
+				obj.click();
+			    clearInterval(timer);
+			    interval = (interval < min_speed) ? min_speed : parseInt(interval/divisor);
+			    timer = setInterval(stepper, interval);
+			}
+
+			timer = setInterval(stepper, interval);
+		});
+
+		obj.on('mouseup mouseleave', function()
+		{
+			clearInterval(timer);
+		});
+	});
+
+	return this;
+}
